62fd1e1439d41a048f984088269e0e1d45dd0b38
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-17 08:58:42 +0200

    add place items page and list widget with navigation to add place screen

--- lib/src/features/places/place_items/ui/pages/place_items_page.dart
+++ lib/src/features/places/place_items/ui/pages/place_items_page.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+
+import '../../../../../core/models/add_places.dart';
 import '../../../../main/ui/custom_app_bar.dart';
+import '../widgets/place_items_list.dart';
 
 class PlaceItemsPage extends StatelessWidget {
   const PlaceItemsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(
         appBarTitle: "Place Collection Title",
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            child: const Text(
+              "Add",
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Text("Place Items"),
-      ),
+      body: PlaceItemsList(),
     );
   }
 }
