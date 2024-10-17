c060d1ce68b3718879ad2998e535081bd790f074
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-17 08:50:20 +0200

    refactor so callback and button uses same function

--- lib/src/features/places/add_place_collection/ui/pages/add_place_collection_page.dart
+++ lib/src/features/places/add_place_collection/ui/pages/add_place_collection_page.dart
@@ -4,41 +4,45 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../core/constants/app_sizes.dart';
 import '../../../../main/ui/custom_app_bar.dart';
-import '../../providers/add_place_collection_form_controller.dart';
+import '../../../place_collections/providers/place_collection_list_provider.dart';
+import '../../providers/add_place_collection_form_provider.dart';
 import '../widgets/add_place_collection_form.dart';
 
-class AddPlaceCollectionPage extends StatelessWidget {
+class AddPlaceCollectionPage extends ConsumerWidget {
   const AddPlaceCollectionPage({super.key});
 
+  void _addPlaceCollection(BuildContext context, WidgetRef ref, String title) {
+    ref
+        .read(placeCollectionListProvider.notifier)
+        .addPlaceCollection(title);
+    Navigator.of(context).pop();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AddPlaceCollectionFormState formState =
+        ref.watch(addPlaceCollectionFormProvider);
+
     return Scaffold(
       appBar: CustomAppBar(
-        appBarTitle: "Add Place Collection",
+        appBarTitle: "Add Collection",
         actions: [
-          Consumer(
-            builder: (_, WidgetRef ref, __) {
-              final AddPlaceCollectionFormState formState =
-                  ref.watch(addPlaceCollectionFormProvider);
-
-              return TextButton(
-                onPressed: formState.isValid
-                    ? () {
-                        Navigator.of(context).pop();
-                      }
-                    : null,
-                child: const Text(
-                  "Add",
-                  style: TextStyle(fontSize: 20),
-                ),
-              );
-            },
+          TextButton(
+            onPressed: formState.isValid
+                ? () => _addPlaceCollection(context, ref, formState.title)
+                : null,
+            child: const Text(
+              "Add",
+              style: TextStyle(fontSize: 20),
+            ),
           ),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: AppSizes.s20),
-        child: AddPlaceCollectionForm(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: AppSizes.s20),
+        child: AddPlaceCollectionForm(
+          onSubmit: () => _addPlaceCollection(context, ref, formState.title),
+        ),
       ),
     );
   }
