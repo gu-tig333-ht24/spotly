7e84a3ceec8bc94f67326b80d84336daf41c0250
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-17 06:15:00 +0200

    extend CustomAppBar to take in actions optionally

--- lib/src/features/main/ui/custom_app_bar.dart
+++ lib/src/features/main/ui/custom_app_bar.dart
@@ -4,10 +4,12 @@ import '../../../core/constants/app_constants.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String appBarTitle;
+  final List<Widget>? actions;
 
   const CustomAppBar({
     super.key,
     this.appBarTitle = AppConstants.appTitle,
+    this.actions,
   });
 
   @override
@@ -15,6 +17,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       centerTitle: true,
       title: Text(appBarTitle),
+      actions: actions,
     );
   }
 
