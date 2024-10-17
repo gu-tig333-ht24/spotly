d1ae52462241983377e5b0b3cc3f6ef1ada551d7
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-17 08:49:31 +0200

    rename files

---
+++ lib/src/features/places/place_collections/providers/place_collection_list_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../../../core/models/place_collection.dart';
+
+final placeCollectionListProvider = StateNotifierProvider.autoDispose<
+    PlaceCollectionListController, AsyncValue<List<PlaceCollection>>>((ref) {
+  return PlaceCollectionListController();
+});
+
+class PlaceCollectionListController
+    extends StateNotifier<AsyncValue<List<PlaceCollection>>> {
+  PlaceCollectionListController() : super(const AsyncValue.data([]));
+
+  Future<void> addPlaceCollection(String title) async {
+    final List<PlaceCollection> currentCollections = List.from(
+      state.value ?? [],
+    );
+
+    try {
+      currentCollections.add(
+        PlaceCollection(
+          id: const Uuid().v4(),
+          title: title,
+          createdAt: DateTime.now(),
+        ),
+      );
+      state = AsyncData(currentCollections);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+
+      if (kDebugMode) {
+        debugPrint("❌ -> addPlaceCollection(), error: $e");
+      }
+    }
+  }
+}
