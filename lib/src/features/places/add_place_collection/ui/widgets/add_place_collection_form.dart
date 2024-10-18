d4f3b948cb9f2f35aa60f66cc398a2bb13e9acc2
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-18 15:12:53 +0200

    improve form ui

--- lib/src/features/places/add_place_collection/ui/widgets/add_place_collection_form.dart
+++ lib/src/features/places/add_place_collection/ui/widgets/add_place_collection_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../core/constants/app_constants.dart';
+import '../../../../../core/constants/app_sizes.dart';
 import '../../providers/add_place_collection_form_provider.dart';
 
 class AddPlaceCollectionForm extends ConsumerStatefulWidget {
@@ -22,20 +23,20 @@ class _AddPlaceCollectionFormState
     extends ConsumerState<AddPlaceCollectionForm> {
   late final AddPlaceCollectionFormController _formController;
   late final TextEditingController _titleController;
-  late final TextEditingController _descriptionController;  // Add description controller
+  late final TextEditingController _descriptionController;
 
   @override
   void initState() {
     super.initState();
     _formController = ref.read(addPlaceCollectionFormProvider.notifier);
     _titleController = TextEditingController();
-    _descriptionController = TextEditingController();  // Initialize description controller
+    _descriptionController = TextEditingController();
   }
 
   @override
   void dispose() {
     _titleController.dispose();
-    _descriptionController.dispose();  // Dispose description controller
+    _descriptionController.dispose();
     super.dispose();
   }
 
@@ -44,88 +45,116 @@ class _AddPlaceCollectionFormState
     final AddPlaceCollectionFormState formState =
         ref.watch(addPlaceCollectionFormProvider);
 
-    return Column(
-      children: [
-        TextFormField(
-          onChanged: _formController.changeTitle,
-          onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
-          onFieldSubmitted:
-              formState.isValid ? (_) => widget.onSubmit?.call() : null,
-          controller: _titleController,
-          cursorColor: Colors.deepPurpleAccent,
-          keyboardType: TextInputType.text,
-          textCapitalization: TextCapitalization.none,
-          textInputAction: TextInputAction.done,
-          maxLines: 1,
-          // Customize the input text style
-          style: const TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-            hintText: "Title",
-            // Customize the placeholder text style
-            hintStyle: const TextStyle(
-              color: Colors.white54,
-              // fontSize: 16.0,
-            ),
-            suffixIcon: IconButton(
-              onPressed: () {
-                _titleController.clear();
-              },
-              icon: const Icon(
-                Icons.clear,
-                color: Colors.deepPurpleAccent,
+    return Form(
+      child: Column(
+        children: [
+          const SizedBox(height: AppSizes.s10),
+          TextFormField(
+            onChanged: _formController.changeTitle,
+            onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+            onFieldSubmitted:
+                formState.isValid ? (_) => widget.onSubmit?.call() : null,
+            controller: _titleController,
+            cursorColor: Colors.deepPurpleAccent,
+            keyboardType: TextInputType.text,
+            textCapitalization: TextCapitalization.none,
+            textInputAction: TextInputAction.done,
+            maxLines: 1,
+            // Customize the input text style
+            style: const TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              labelText: "Title*",
+              labelStyle: const TextStyle(
+                color: Colors.white70,
+                fontSize: 24,
               ),
-              tooltip: "Clear",
-            ),
-            // The style for the border by default and enabled
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.deepPurple,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  _titleController.clear();
+                },
+                icon: const Icon(
+                  Icons.clear,
+                  color: Colors.redAccent,
+                ),
+                tooltip: "Clear",
               ),
-              borderRadius: BorderRadius.circular(AppConstants.borderRadius),
-            ),
-            // The style for the border when focused
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.deepPurpleAccent,
+              // The style for the border by default and enabled
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.deepPurple,
+                ),
+                borderRadius: BorderRadius.circular(AppConstants.borderRadius),
               ),
-              borderRadius: BorderRadius.circular(AppConstants.borderRadius),
+              // // The style for the border when focused
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.deepPurpleAccent,
+                ),
+                borderRadius: BorderRadius.circular(AppConstants.borderRadius),
               ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        // Add new TextFormField for description
-        TextFormField(
-          onChanged: _formController.changeDescription,  // Handle description changes
-          onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
-          onFieldSubmitted:
-              formState.isValid ? (_) => widget.onSubmit?.call() : null,
-          controller: _descriptionController,
-          cursorColor: Colors.deepPurpleAccent,
-          keyboardType: TextInputType.text,
-          textCapitalization: TextCapitalization.sentences,
-          textInputAction: TextInputAction.done,
-          maxLines: 3,
-          style: const TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-            hintText: "Description",
-            hintStyle: const TextStyle(
-              color: Colors.white54,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.deepPurple,
-              ),
-              borderRadius: BorderRadius.circular(AppConstants.borderRadius),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.deepPurpleAccent,
-              ),
-              borderRadius: BorderRadius.circular(AppConstants.borderRadius),
             ),
           ),
-        ),
-      ],
+          const SizedBox(height: AppSizes.s20),
+          // Add new TextFormField for description
+          TextFormField(
+            onChanged: _formController.changeDescription,
+            onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+            onFieldSubmitted:
+                formState.isValid ? (_) => widget.onSubmit?.call() : null,
+            controller: _descriptionController,
+            cursorColor: Colors.deepPurpleAccent,
+            keyboardType: TextInputType.text,
+            textCapitalization: TextCapitalization.sentences,
+            textInputAction: TextInputAction.done,
+            maxLines: 3,
+            style: const TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              labelText: "Description",
+              labelStyle: const TextStyle(
+                color: Colors.white70,
+                fontSize: 24,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.deepPurple,
+                ),
+                borderRadius: BorderRadius.circular(AppConstants.borderRadius),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.deepPurpleAccent,
+                ),
+                borderRadius: BorderRadius.circular(AppConstants.borderRadius),
+              ),
+            ),
+          ),
+          const SizedBox(height: AppSizes.s20),
+          Row(
+            children: [
+              Expanded(
+                child: FilledButton(
+                  onPressed:
+                      formState.isValid ? () => widget.onSubmit?.call() : null,
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.deepPurpleAccent,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(AppConstants.borderRadius),
+                    ),
+                  ),
+                  child: const Text(
+                    "Save",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
