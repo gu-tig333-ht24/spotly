0d20edafc0267518fee3b2f577a056db62a2e4a9
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-18 15:11:18 +0200

    refactor to use database service and implement most of crud functions

--- lib/src/features/places/place_collections/providers/place_collection_list_provider.dart
+++ lib/src/features/places/place_collections/providers/place_collection_list_provider.dart
@@ -1,38 +1,85 @@
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:uuid/uuid.dart';
 
 import '../../../../core/models/place_collection.dart';
+import '../../../../data/providers/database_providers.dart';
+import '../../../../data/services/database_service.dart';
 
-final placeCollectionListProvider = StateNotifierProvider.autoDispose<
+final placeCollectionListProvider = StateNotifierProvider<
     PlaceCollectionListController, AsyncValue<List<PlaceCollection>>>((ref) {
-  return PlaceCollectionListController();
+  final DatabaseService service = ref.read(databaseServiceProvider);
+  return PlaceCollectionListController(databaseService: service);
 });
 
 class PlaceCollectionListController
     extends StateNotifier<AsyncValue<List<PlaceCollection>>> {
-  PlaceCollectionListController() : super(const AsyncValue.data([]));
+  PlaceCollectionListController({
+    required DatabaseService databaseService,
+  })  : _databaseService = databaseService,
+        super(const AsyncValue.data([]));
+
+  final DatabaseService _databaseService;
 
   Future<void> addPlaceCollection(String title, String description) async {
+    final PlaceCollection newCollection =
+        await _databaseService.createPlaceCollection(
+      title,
+      description,
+    );
+
     final List<PlaceCollection> currentCollections = List.from(
       state.value ?? [],
     );
 
     try {
-      currentCollections.add(
-        PlaceCollection(
-          id: const Uuid().v4(),
-          title: title,
-          createdAt: DateTime.now(),
-          //description: description, // description parameter
-          description: 'No description provided', // Default description
-        ),
-      );
+      currentCollections.add(newCollection);
       state = AsyncData(currentCollections);
     } catch (e, st) {
       state = AsyncError(e, st);
 
+      if (kDebugMode) {
+        debugPrint("❌ -> addPlaceCollection(), error: $e");
+      }
+    }
+  }
+
+  Future<void> retrieveCollections() async {
+    state = const AsyncLoading();
+
+    final List<PlaceCollection> collections =
+        await _databaseService.getPlaceCollections();
+
+    try {
+      state = AsyncData(collections);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+
+      if (kDebugMode) {
+        debugPrint("❌ -> retrieveCollections(), error: $e");
+      }
+    }
+  }
+
+  Future<void> deleteCollection(PlaceCollection collection) async {
+    final deleted = await _databaseService.deletePlaceCollectionById(
+      collection.id,
+    );
+
+    if (!deleted) {
+      // TODO: show snackbar message indicating that something went wrong?
+      return;
+    }
+
+    final List<PlaceCollection> currentCollections = List.from(
+      state.value ?? [],
+    );
+
+    try {
+      currentCollections.remove(collection);
+      state = AsyncData(currentCollections);
+    } catch (e, st) {
+      state = AsyncError(e, st);
       if (kDebugMode) {
         debugPrint("❌ -> addPlaceCollection(), error: $e");
       }
