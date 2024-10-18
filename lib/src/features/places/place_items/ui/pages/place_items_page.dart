d84b38da99fef00a2d3e616d0c5910ff57b7e977
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-18 15:14:48 +0200

    add PlaceCollection field

--- lib/src/features/places/place_items/ui/pages/place_items_page.dart
+++ lib/src/features/places/place_items/ui/pages/place_items_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/models/add_places.dart';
+import '../../../../../core/models/place_collection.dart';
 import '../../../../main/ui/custom_app_bar.dart';
 import '../widgets/place_items_list.dart';
 
 class PlaceItemsPage extends StatelessWidget {
-  const PlaceItemsPage({super.key});
+  const PlaceItemsPage({
+    super.key,
+    required this.collection,
+  });
+
+  final PlaceCollection collection;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-        appBarTitle: "Place Collection Title",
+        appBarTitle: collection.title,
         actions: [
           TextButton(
             onPressed: () {
