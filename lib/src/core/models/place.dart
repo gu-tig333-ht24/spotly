6a47be6467f2e33223e7a837099edf81530f1ec9
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-19 07:15:55 +0200

    remove commented out code

--- lib/src/core/models/place.dart
+++ lib/src/core/models/place.dart
@@ -1,49 +1,3 @@
-import 'package:uuid/uuid.dart';
-import 'dart:io';
-
-const uuid = Uuid();
-
-class Place2 {
-  Place2({
-    required this.title,
-    required this.image, // Använd image som File-objekt
-    required this.description, // Beskrivning av platsen
-    required this.createdAt, // Datum då platsen skapades
-    //required this.location, // Obligatoriskt fält för location
-  }) : id = uuid.v4();
-
-  final String id;
-  final String title;
-  final File image;
-  final String description;
-  final DateTime createdAt;
-  //final PlaceLocation location; // Final variabel för platsens position
-
-  // Konvertera Place-objekt till JSON-format
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'imagePath': image.path, // Använd 'image.path' för att spara filvägen
-      'description': description,
-      'createdAt': createdAt.toIso8601String(), // Spara datum i ISO-format
-      //'location': location.toJson(), // Lägg till platsinformationen
-    };
-  }
-
-  // Skapa ett Place-objekt från JSON
-  factory Place2.fromJson(Map<String, dynamic> json) {
-    return Place2(
-      title: json['title'],
-      image: File(json['imagePath']), // Skapa ett File-objekt från filvägen
-      description: json['description'],
-      createdAt: DateTime.parse(json['createdAt']),
-      //location: PlaceLocation.fromJson(
-      //json['location']), // Återställ platsinformation från JSON
-    );
-  }
-}
-
 class Place {
   Place({
     required this.id,
@@ -60,9 +14,9 @@ class Place {
       collectionId: map["collectionId"] as int,
       title: map.containsKey("title") ? map["title"] : "",
       imagePath:
-      map.containsKey("imagePath") ? map["imagePath"] as String? : null,
+          map.containsKey("imagePath") ? map["imagePath"] as String? : null,
       description:
-      map.containsKey("description") ? map["description"] as String? : null,
+          map.containsKey("description") ? map["description"] as String? : null,
       createdAt: map.containsKey("createdAt")
           ? DateTime.tryParse(map["createdAt"]) ?? DateTime.now()
           : DateTime.now(),
