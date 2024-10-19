3b73c15b37361ac5886fc0f1c9ba8f89550efb9c
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-19 04:56:50 +0200

    modify main

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     const ProviderScope(
       child: MainApp(),
