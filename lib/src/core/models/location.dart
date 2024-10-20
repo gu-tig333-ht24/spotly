d696b9a7ba7218ad52c88ce88be9b86aaa8a0a00
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-20 14:48:54 +0200

    rename to fit dart idiom

--- lib/src/core/models/location.dart
+++ lib/src/core/models/location.dart
@@ -5,23 +5,23 @@ class Location {
     required this.address,
   });
 
-  final double latitude;
-  final double longitude;
-  final String? address;
-
-  Map<String, dynamic> toJson() {
-    return {
-      "latitude": latitude,
-      "longitude": longitude,
-      "address": address,
-    };
-  }
-
-  factory Location.fromJson(Map<String, dynamic> json) {
+  factory Location.fromMap(Map<String, dynamic> json) {
     return Location(
       latitude: json["latitude"],
       longitude: json["longitude"],
       address: json["address"],
     );
+  }
+
+  final double latitude;
+  final double longitude;
+  final String? address;
+
+  Map<String, dynamic> toMap() {
+    return {
+      "latitude": latitude,
+      "longitude": longitude,
+      "address": address,
+    };
   }
 }
