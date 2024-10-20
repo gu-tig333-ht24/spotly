e1543f5c4d5a84683c7b2b6f29c5f29d6f4dac99
Filip Drincic <fildri0903@outlook.com>
2024-10-20 21:33:03 +0200

    Comments update for settings

--- lib/src/features/settings/ui/pages/privacy_security_page.dart
+++ lib/src/features/settings/ui/pages/privacy_security_page.dart
@@ -18,23 +18,23 @@ class PrivacySecurityPage extends StatelessWidget {
               'Privacy:',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
-            const SizedBox(height: 10), // Lägger till mellanrum
+            const SizedBox(height: 10), 
             Text(
               'Spotly stores all data locally on your device. If you are logged into iCloud and have enabled its backup option or the Spotly iCloud sync feature, your data will be backed up to your personal iCloud account. You can manage your backup settings directly in your iCloud preferences.',
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     height: 1.5,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
-            const SizedBox(height: 20), // Avstånd mellan textblock
+            const SizedBox(height: 20), // Distance between textblocks
             Text(
               'Data security:',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             const SizedBox(height: 10),
@@ -42,7 +42,7 @@ class PrivacySecurityPage extends StatelessWidget {
               'Spotly does not collect any personal data. However, the app uses Google Maps, which may collect location information as part of its functionality. For more information on how Google Maps handles your data, please refer to Google\'s Privacy Policy.',
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     height: 1.5,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             const SizedBox(height: 20),
@@ -50,7 +50,7 @@ class PrivacySecurityPage extends StatelessWidget {
               'Security measures:',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             const SizedBox(height: 10),
@@ -58,7 +58,7 @@ class PrivacySecurityPage extends StatelessWidget {
               'All data transmitted between the app and external services, such as iCloud, is encrypted to ensure your data is protected during transmission.',
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     height: 1.5,
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
           ],
