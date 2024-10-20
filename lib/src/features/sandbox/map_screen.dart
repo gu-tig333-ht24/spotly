156a2aac323ad285147a6f54658497ea57c5f66a
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-20 16:38:26 +0200

    add map screen to source control and fix incompatibilities

---
+++ lib/src/features/sandbox/map_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../../core/models/location.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.location = const Location(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: '',
+    ),
+    this.isSelecting = true,
+  });
+
+  final Location location;
+  final bool isSelecting;
+
+  @override
+  State<MapScreen> createState() {
+    return _MapScreenState();
+  }
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+  GoogleMapController? _mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
+
+  void _checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Hantera om platsstjänster inte är aktiverade
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Hantera om behörighet nekas
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Hantera om behörighet nekas permanent
+      return;
+    }
+
+    // Om behörigheter är okej, initiera kartan
+    setState(() {
+      _mapController?.animateCamera(
+        CameraUpdate.newLatLng(
+          LatLng(widget.location.latitude, widget.location.longitude),
+        ),
+      );
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(widget.isSelecting ? 'Pick your Location' : 'Your Location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () {
+                Navigator.of(context).pop(_pickedLocation);
+              },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        onTap: (position) {
+          setState(() {
+            _pickedLocation = position;
+          });
+        },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.location.latitude,
+            widget.location.longitude,
+          ),
+          zoom: 16,
+        ),
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: _pickedLocation ??
+                      LatLng(
+                        widget.location.latitude,
+                        widget.location.longitude,
+                      ),
+                ),
+              },
+      ),
+    );
+  }
+}
