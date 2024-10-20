7ddf5cb9dc73eab42a8bf35cf44f64690b09d9f8
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-20 10:55:45 +0200

    sort imports

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/constants/app_constants.dart';
-import 'core/theme/app_theme.dart';
-import 'features/collection/collections/ui/pages/collection_list_page.dart';
 import 'core/enums/bottom_tab.dart';
 import 'core/providers/app_state_provider.dart';
+import 'core/providers/dark_mode_provider.dart';
+import 'core/theme/app_theme.dart';
+import 'features/collection/collections/ui/pages/collection_list_page.dart';
 import 'features/search/ui/pages/search_page.dart';
 import 'features/settings/ui/pages/settings_page.dart';
-import 'core/providers/dark_mode_provider.dart'; 
 
 class MainApp extends ConsumerWidget {
   const MainApp({super.key});
@@ -22,7 +22,7 @@ class MainApp extends ConsumerWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppConstants.appTitle,
-      theme: isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme, // Change theme based on dark mode 
+      theme: isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme, // Change theme based on dark mode
       home: switch (appState.selectedTab) {
         BottomTab.places => const CollectionListPage(),
         BottomTab.search => const SearchPage(),
