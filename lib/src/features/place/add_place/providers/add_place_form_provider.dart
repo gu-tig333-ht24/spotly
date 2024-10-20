ad50c6d7582bce84850ddbbc5d977e72a19e4ec3
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-20 13:33:39 +0200

    refactor form state provider to handle location and image file

--- lib/src/features/place/add_place/providers/add_place_form_provider.dart
+++ lib/src/features/place/add_place/providers/add_place_form_provider.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/models/location.dart';
 
 final addPlaceFormProvider = StateNotifierProvider.autoDispose<
     AddPlaceFormController, AddPlaceFormState>((ref) {
@@ -21,6 +25,14 @@ class AddPlaceFormController extends StateNotifier<AddPlaceFormState> {
   void changeImagePath(String? newImagePath) {
     state = state.copyWith(imagePath: newImagePath);
   }
+
+  void changeLocation(Location? newLocation) {
+    state = state.copyWith(location: newLocation);
+  }
+
+  void changeSelectedImageFile(File? newFile) {
+    state = state.copyWith(selectedImageFile: newFile);
+  }
 }
 
 @immutable
@@ -29,17 +41,24 @@ class AddPlaceFormState {
     required this.title,
     required this.description,
     this.imagePath,
+    this.location,
+    this.selectedImageFile,
   });
 
   factory AddPlaceFormState.initial() => const AddPlaceFormState(
         title: "",
         description: "",
         imagePath: null,
+        location: null,
+        selectedImageFile: null,
       );
 
   final String title;
   final String description;
   final String? imagePath;
+  final Location? location;
+
+  final File? selectedImageFile;
 
   bool get isValid => title.isNotEmpty;
 
@@ -47,11 +66,15 @@ class AddPlaceFormState {
     String? title,
     String? description,
     String? imagePath,
+    Location? location,
+    File? selectedImageFile,
   }) {
     return AddPlaceFormState(
       title: title ?? this.title,
       description: description ?? this.description,
       imagePath: imagePath ?? this.imagePath,
+      location: location ?? this.location,
+      selectedImageFile: selectedImageFile ?? this.selectedImageFile,
     );
   }
 }
