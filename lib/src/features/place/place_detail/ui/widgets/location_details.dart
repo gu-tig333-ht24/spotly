fe1102d4da09c1bb4841ffa997d67c68e788fdc8
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-22 17:27:09 +0200

    add button to launch location to native map app

--- lib/src/features/place/place_detail/ui/widgets/location_details.dart
+++ lib/src/features/place/place_detail/ui/widgets/location_details.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/constants/app_sizes.dart';
 import '../../../../../core/models/location.dart';
+import '../../../../../core/widgets/custom_icon_button.dart';
 import 'location_rich_text.dart';
 
 class LocationDetails extends StatelessWidget {
@@ -12,16 +17,50 @@ class LocationDetails extends StatelessWidget {
 
   final Location location;
 
+  Future<void> _launchMap() async {
+    final lat = location.latitude;
+    final lon = location.longitude;
+    final address = location.address ?? "Unknown address";
+
+    final Uri url;
+    if (Platform.isIOS) {
+      url = Uri.parse("maps:$lat,$lon?q=$lat,$lon");
+    } else if (Platform.isAndroid) {
+      url = Uri.parse("geo:$lat,$lon?q=$lat,$lon($address)");
+    } else {
+      // Not supported.
+      return;
+    }
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(
-          "Location",
-          style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
+        Row(
+          children: [
+            Text(
+              "Location",
+              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            Tooltip(
+              message: "Launch in ${Platform.isIOS ? "Apple" : "Google"} Maps",
+              preferBelow: true,
+              child: Platform.isIOS || Platform.isAndroid
+                  ? CustomIconButton(
+                      icon: Icons.map_rounded,
+                      onPressed: _launchMap,
+                    )
+                  : null,
+            ),
+          ],
         ),
         const SizedBox(height: AppSizes.s10),
         LocationRichText(
